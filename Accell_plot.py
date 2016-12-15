612f763d4c790ece1f5718cc4753799ac981017d
fredvol <fredcab66@hotmail.com>
2016-12-15 20:31:48 +0100

    improve Graph

--- Accell_plot.py
+++ Accell_plot.py
@@ -4,14 +4,13 @@ Created on Sat Dec 10 09:34:45 2016
 
 @author: fred
 """
-import matplotlib
 
 from matplotlib import pyplot as plt
 from matplotlib import style
 import numpy as np
 
 style.use('ggplot')
-path="RecACC_20161214193205.txt"
+path="Data/RecACC_20161214193205.txt"
 
 time= np.loadtxt(path,
                  delimiter = ',',
@@ -31,31 +30,38 @@ accz= np.loadtxt(path,
                  usecols = (5,))
 
 
-#plt.plot(time,accx)
-#plt.plot(time,accy)
-#plt.plot(time,accz)
-#
-#plt.title('Acc X Y Z')
-#plt.ylabel('Acc')
-#plt.xlabel('Time')
-#
-#plt.show()
 
-fig = plt.figure()
 
-ax1 = fig.add_subplot(311)
+accVec= np.array([accx,accy,accz])
+accT= np.linalg.norm(accVec)
+
+
+# PLOT GRAPH
+
+f, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=True)
+
+
 ax1.plot(time,accx)
-
-ax2 = fig.add_subplot(312)
 ax2.plot(time,accy)
-
-ax3 = fig.add_subplot(313)
 ax3.plot(time,accz)
 
 
+
+
 # Add label:
+ax1.set_ylabel("Accel X")
+ax2.set_ylabel("Accel Y")
+ax3.set_ylabel("Accel Z")
+
+plt.xlabel('Time (ms) ')
+
+
+f.subplots_adjust(hspace=0)
+f.suptitle('Acceleration', size=16)
 
 
 
 
-plt.show()
+
+
+f.show()
