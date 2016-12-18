e9234a3948e0d65647deba3d97cd377256289ab9
fred <fredcab70@outlook.fr>
2016-12-18 11:53:44 +0100

    add def

--- Accell_plot.py
+++ Accell_plot.py
@@ -9,7 +9,36 @@ from matplotlib import pyplot as plt
 from matplotlib import style
 import numpy as np
 
-## Import data
+
+#==============================================================================
+# FUNCTION
+#==============================================================================
+
+def filterOrder1( mylist ):
+   "This changes a passed list into this function"
+   # Filter Accx Order 1
+   mylistF1= [0.0] * len(mylist)
+   mylistF1[0]=  (mylist[0])
+
+   #◘ treatement for Acc X
+   for i in range(1, len(mylist)-1):
+#    print("i= ", i)
+#    print("accxF1[i-1] ", accxF1[i-1])
+#    print("accx[i] ", accx[i])
+        mylistF1[i]= (F1param1*mylistF1[i-1]) + ((1-F1param1)*mylist[i])
+   
+   return mylistF1;
+   
+   
+## AVerage 10
+def avg10( mylist ):
+    submylist = mylist[1:10]
+    mylistAvg=sum(submylist)/len(submylist)
+    return mylistAvg;
+
+#==============================================================================
+# Import data
+#==============================================================================
 style.use('ggplot')
 path="Data/RecACC_20161214193205.txt"
 
@@ -36,37 +65,62 @@ time0=time[0]
 timeS = [(x - time0)/1000000000 for x in time]
 
 
-## FILTER
-# Filter Accx Order 1
-
-accxF1= [0.0] * len(accx)
-accxF1[0]=  (accx[0])
+#==============================================================================
+# ## FILTER
+#==============================================================================
 
 #Parameter 
 F1param1=0.8
 F1limit1=3
 
-#◘ treatement for Acc X
-for i in range(1, len(accx)-1):
-    print("i= ", i)
-    print("accxF1[i-1] ", accxF1[i-1])
-    print("accx[i] ", accx[i])
-    accxF1[i]= (F1param1*accxF1[i-1]) + ((1-F1param1)*accx[i])
+# Filter Order 1
+
+accxF1 = filterOrder1(accx)
+accyF1 = filterOrder1(accy)
+acczF1 = filterOrder1(accz)
+
+
+#==============================================================================
+# Find limits
+#==============================================================================
+
+AccXF1Avg=avg10(accxF1)
+AccYF1Avg=avg10(accyF1)
+AccZF1Avg=avg10(acczF1)
+
+AccXUpper = AccXF1Avg + F1limit1
+AccXLower = AccXF1Avg - F1limit1
+
+AccYUpper = AccYF1Avg + F1limit1
+AccYLower = AccYF1Avg - F1limit1
+
+AccZUpper = AccZF1Avg + F1limit1
+AccZLower = AccZF1Avg - F1limit1
 
 
 
-## PLOT GRAPH
+#==============================================================================
+# ## PLOT GRAPH
+#==============================================================================
 
 f, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=True)
 
 
 ax1.plot(timeS,accx)
 ax1.plot(timeS,accxF1)
-ax1.axhline(y=F1limit1, xmin=0, xmax=timeS[-1], linewidth=1, color = 'g')
-ax1.axhline(y=-F1limit1, xmin=0, xmax=timeS[-1], linewidth=1, color = 'g')
+ax1.axhline(y=AccXUpper, xmin=0, xmax=timeS[-1], linewidth=1, color = 'g')
+ax1.axhline(y=AccXLower, xmin=0, xmax=timeS[-1], linewidth=1, color = 'g')
+
 
 ax2.plot(timeS,accy)
+ax2.plot(timeS,accyF1)
+ax2.axhline(y=AccYUpper, xmin=0, xmax=timeS[-1], linewidth=1, color = 'g')
+ax2.axhline(y=AccYLower, xmin=0, xmax=timeS[-1], linewidth=1, color = 'g')
+
 ax3.plot(timeS,accz)
+ax3.plot(timeS,acczF1)
+ax3.axhline(y=AccZUpper, xmin=0, xmax=timeS[-1], linewidth=1, color = 'g')
+ax3.axhline(y=AccZLower, xmin=0, xmax=timeS[-1], linewidth=1, color = 'g')
 
 
 
@@ -85,3 +139,8 @@ f.suptitle('Acceleration (m/s^2)', size=16)
 
 
 f.show()
+
+
+
+
+
